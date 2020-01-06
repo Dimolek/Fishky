a6772a86d8c092011fed484a1c913e8deb404b4e
Dimol <dimolek@gmail.com>
2020-01-06 14:32:15 +0100

    Initial commit

---
+++ src/test/java/com/fishky/FishkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
