ee97390bece8033019a0f4fdb7cdae5fd268cb9d
Mat <msochacki5@gmail.com>
2020-02-03 18:40:13 +0100

    frontend init commit

--- src/main/java/com/fishky/controller/DictionaryController.java
+++ src/main/java/com/fishky/controller/DictionaryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class DictionaryController {
 
     public static final String ENTITY_ID = "id";
